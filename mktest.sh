477a316dcb2c368007bf930d79804c6b14d6c8c7
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2012-08-22 11:03:07 -0600

    Switch if from zero to urandom for read/write testing

--- mktest.sh
+++ mktest.sh
@@ -6,7 +6,7 @@ do
     mkdir $i;
     for j in {1..10000};
     do
-        dd if=/dev/zero of=$i/$j bs=8k count=1 &> /dev/null;
+        dd if=/dev/urandom of=$i/$j bs=8k count=1 &> /dev/null;
     done
 done
 
