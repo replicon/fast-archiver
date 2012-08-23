42e27d5f3844544d3292fbb4da4e4fd65ad98bf5
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2012-08-23 06:45:34 -0600

    add random file sizes

--- mktest.sh
+++ mktest.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for i in {1..1000};
+for i in {1..10};
 do
     echo $i;
     mkdir $i;
     for j in {1..10000};
     do
-        dd if=/dev/urandom of=$i/$j bs=8k count=1 &> /dev/null;
+        let "x = 4 + 4 * ($RANDOM % 4)"
+        dd if=/dev/urandom of=$i/$j bs=${x}k count=1 &> /dev/null;
     done
 done
 
